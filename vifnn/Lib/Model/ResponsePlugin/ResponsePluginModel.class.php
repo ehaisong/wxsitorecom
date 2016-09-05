eb4ca85c028324aa1a0f8169994eff1d43d0210b
Jun <easelify@gmail.com>
2016-09-05 23:54:05 +0800

    回复插件:进一步完善后端程序框架，增加文本消息回复处理接口

--- vifnn/Lib/Model/ResponsePlugin/ResponsePluginModel.class.php
+++ vifnn/Lib/Model/ResponsePlugin/ResponsePluginModel.class.php
@@ -30,23 +30,40 @@ class ResponsePluginModel extends Model
 
     /**
      * 运行插件回复
+     * 目前插件只支持 text, event 两类消息进行插件回复
      * @return mixed
      */
     public function run()
     {
-        if (empty($wxuser) || empty($data)) {
+        if (empty($this->wxuser) || empty($this->data)) {
             return false;
         }
 
-        // 根据关键字找出可用的插件
-        if ($data['MsgType'] == 'text') {
+        // 文本消息回复
+        if ($this->data['MsgType'] == 'text') {
             $plugin = M('affiliate_response_plugin')->where(array(
                 'wxuser_id' => $this->wxuser['id'],
                 'is_enable' => 1,
-                'keyword' => $data['Content'],
-            ))->order('`plugin_id` asc')->find();
+                'keyword' => $this->data['Content'],
+            ))->order('`plugin_id` asc')->limit(0, 1)->find();
 
-            return array(join(',', $plugin), 'text');
+            if (empty($plugin)) {
+                return false;
+            }
+
+            $plugin_model_name = $plugin['model_name'] . 'Model';
+            import('@.Model.ResponsePlugin.' . $plugin_model_name);
+            if (class_exists($plugin_model_name)) {
+                $model = new $plugin_model_name($this->wxuser, $this->data);
+                return $model->reply();
+            } else {
+                return array('插件【' . $plugin['plugin_name'] . '】核心响应文件不存在，请联系客服处理！', 'text');
+            }
+        }
+
+        // 事件消息回复
+        if ($this->data['MsgType'] == 'event') {
+            // ...
         }
     }
 
